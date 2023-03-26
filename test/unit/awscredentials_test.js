7a36defc5fbb6809a2cf2c92f83c4f6ebbfd8a4b
Shawn Kim <shawnhan.kim@gmail.com>
2023-03-26 05:53:01 -0700

    feat: fetch credentials via EC2, ECS, EKS fix: call to awscredentials.fetchCredentials from nginx location fix: NOW chore: remove unused constant
    fix: s3 request params for signature v4
    
    fix: test parseArray()

--- test/unit/awscredentials_test.js
+++ test/unit/awscredentials_test.js
@@ -19,6 +19,8 @@
 import awscred from "include/awscredentials.js";
 import fs from "fs";
 
+globalThis.ngx = {};
+
 
 function testReadCredentialsWithAccessSecretKeyAndSessionTokenSet() {
     printHeader('testReadCredentialsWithAccessSecretKeyAndSessionTokenSet');
@@ -176,7 +178,128 @@ function testReadAndWriteCredentialsFromKeyValStore() {
     }
 }
 
+async function testEcsCredentialRetrieval() {
+    printHeader('testEcsCredentialRetrieval');
+    process.env['S3_ACCESS_KEY_ID'] = undefined;
+    process.env['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI'] = '/example';
+    globalThis.ngx.fetch = function (url) {
+        globalThis.recordedUrl = url;
+
+        return Promise.resolve({
+            ok: true,
+            json: function () {
+                return Promise.resolve({
+                    AccessKeyId: 'AN_ACCESS_KEY_ID',
+                    Expiration: '2017-05-17T15:09:54Z',
+                    RoleArn: 'TASK_ROLE_ARN',
+                    SecretAccessKey: 'A_SECRET_ACCESS_KEY',
+                    Token: 'A_SECURITY_TOKEN',
+                });
+            }
+        });
+    };
+    var r = {
+        "headersOut" : {
+            "Accept-Ranges": "bytes",
+            "Content-Length": 42,
+            "Content-Security-Policy": "block-all-mixed-content",
+            "Content-Type": "text/plain",
+            "X-Amz-Bucket-Region": "us-east-1",
+            "X-Amz-Request-Id": "166539E18A46500A",
+            "X-Xss-Protection": "1; mode=block"
+        },
+        log: function(msg) {
+            console.log(msg);
+        },
+        return: function(code) {
+            if (code !== 200) {
+                throw 'Expected 200 status code, got: ' + code;
+            }
+        },
+    };
+
+    await awscred.fetchCredentials(r);
+
+    if (globalThis.recordedUrl !== 'http://169.254.170.2/example') {
+        throw 'No or wrong ECS credentials fetch URL recorded: ' + globalThis.recordedUrl;
+    }
+}
+
+async function testEc2CredentialRetrieval() {
+    printHeader('testEc2CredentialRetrieval');
+    process.env['S3_ACCESS_KEY_ID'] = undefined;
+    process.env['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI'] = undefined;
+    globalThis.ngx.fetch = function (url, options) {
+        if (url === 'http://169.254.169.254/latest/api/token' && options && options.method === 'PUT') {
+            return Promise.resolve({
+                ok: true,
+                text: function () {
+                    return Promise.resolve('A_TOKEN');
+                },
+            });
+        } else if (url === 'http://169.254.169.254/latest/meta-data/iam/security-credentials/') {
+            if (options && options.headers && options.headers['x-aws-ec2-metadata-token'] === 'A_TOKEN') {
+                return Promise.resolve({
+                    ok: true,
+                    text: function () {
+                        return Promise.resolve('A_ROLE_NAME');
+                    },
+                });
+            } else {
+                throw 'Invalid token passed: ' + options.headers['x-aws-ec2-metadata-token'];
+            }
+        }  else if (url === 'http://169.254.169.254/latest/meta-data/iam/security-credentials/A_ROLE_NAME') {
+            if (options && options.headers && options.headers['x-aws-ec2-metadata-token'] === 'A_TOKEN') {
+                return Promise.resolve({
+                    ok: true,
+                    json: function () {
+                        globalThis.credentialsIssued = true;
+                        return Promise.resolve({
+                            AccessKeyId: 'AN_ACCESS_KEY_ID',
+                            Expiration: '2017-05-17T15:09:54Z',
+                            RoleArn: 'TASK_ROLE_ARN',
+                            SecretAccessKey: 'A_SECRET_ACCESS_KEY',
+                            Token: 'A_SECURITY_TOKEN',
+                        });
+                    },
+                });
+            } else {
+                throw 'Invalid token passed: ' + options.headers['x-aws-ec2-metadata-token'];
+            }
+        } else {
+            throw 'Invalid request URL: ' + url;
+        }
+    };
+    var r = {
+        "headersOut" : {
+            "Accept-Ranges": "bytes",
+            "Content-Length": 42,
+            "Content-Security-Policy": "block-all-mixed-content",
+            "Content-Type": "text/plain",
+            "X-Amz-Bucket-Region": "us-east-1",
+            "X-Amz-Request-Id": "166539E18A46500A",
+            "X-Xss-Protection": "1; mode=block"
+        },
+        log: function(msg) {
+            console.log(msg);
+        },
+        return: function(code) {
+            if (code !== 200) {
+                throw 'Expected 200 status code, got: ' + code;
+            }
+        },
+    };
+
+    await awscred.fetchCredentials(r);
+
+    if (!globalThis.credentialsIssued) {
+        throw 'Did not reach the point where EC2 credentials were issues.';
+    }
+}
+
 async function test() {
+    await testEc2CredentialRetrieval();
+    await testEcsCredentialRetrieval();
     testReadCredentialsWithAccessSecretKeyAndSessionTokenSet();
     testReadCredentialsFromFilePath();
     testReadCredentialsFromNonexistentPath();
