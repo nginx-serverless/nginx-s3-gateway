c89b3b607d33dbcdf85a6385a9d69593ae967bab
Shawn Kim <shawnhan.kim@gmail.com>
2023-03-26 21:27:17 -0700

    chore: remove dependency with s3 lib from signature lib test code

--- test/unit/awssig4_test.js
+++ test/unit/awssig4_test.js
@@ -67,9 +67,11 @@ function _runSignatureV4(r) {
     var service = 's3';
     var server = 's3-us-west-2.amazonaws.com';
 
-    // TODO: Generate request parameters without using s3gateway to only test 
-    //       awssig4.js for the purpose of common library.
-    let req = s3gateway._s3ReqParamsForSigV4(r, bucket, server);
+    const req = {
+        uri : r.variables.uri_path,
+        queryParams : '',
+        host: bucket.concat('.', server)
+    }
     const canonicalRequest = awssig4._buildCanonicalRequest(
         r.method, req.uri, req.queryParams, req.host, amzDatetime, creds.sessionToken);
 
